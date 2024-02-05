3291ed99359523fa40a51bbaa331c5b076efcea4
olqaeremina <157389086+olqaeremina@users.noreply.github.com>
2024-02-05 23:44:39 +0300

    added meta to models

--- library/library_app/models.py
+++ library/library_app/models.py
@@ -4,11 +4,21 @@ from django.db import models
 class Hall(models.Model):
   short_name = models.CharField(max_length=20, verbose_name='Короткое название')
   full_name = models.CharField(max_length=50, verbose_name='Полное название')
-
+  def __str__(self):
+    return f"{self.short_name} {self.full_name}"
+  class Meta:
+    verbose_name_plural = "Залы"
+    verbose_name = "Зал"
+    ordering = ["short_name", "full_name"]
 class Shelf(models.Model):
   number = models.IntegerField(verbose_name='Номер стеллажа')
   hall = models.ForeignKey(Hall, on_delete=models.CASCADE,verbose_name='Зал')
-
+  def __str__(self):
+    return f"{self.number} {self.hall}"
+  class Meta:
+    verbose_name_plural = "Cтеллажи"
+    verbose_name = "Стеллаж"
+    ordering = ["number", "hall"]
 class Book(models.Model):
   name = models.TextField(null=False,blank=True,verbose_name='Название')
   authors = models.TextField(null=False,blank=True,verbose_name='Авторы')
@@ -18,3 +28,9 @@ class Book(models.Model):
   shelf = models.ForeignKey(Shelf, on_delete=models.CASCADE,verbose_name='Стеллаж')
   shelf_number = models.IntegerField(verbose_name='Номер полки')
   amount = models.IntegerField(verbose_name='Кол-во экземпляров')
+  def __str__(self):
+    return f"{self.name} {self.authors} {self.chapter} {self.isbn} {self.publishing_year} {self.amount}"
+  class Meta:
+    verbose_name_plural = "Книги"
+    verbose_name = "Книга"
+    ordering = ["isbn"]
